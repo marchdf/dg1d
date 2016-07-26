fbe4a200a4cc3c4a8e6aaf7fe36dd1837f6f2be0
Marc Henry de Frahan (lab) <marchdf@umich.edu>
2016-07-26 08:19:07 -0400

    Decreased the Rusanov Riemann solver time by a factor of 30.
    Vectorized the operations in the Riemann solver.

--- dg1d/euler_fluxes.py
+++ dg1d/euler_fluxes.py
@@ -49,39 +49,31 @@ def riemann_rusanov(ul,ur):
     # Initialize
     F = np.zeros(ul.shape)
 
-    # Loop on all the elements
-    for i in range(0,len(ul),3):
+    # Primitive variables and sound speeds
+    rhoL = ul[0::3]
+    vL   = ul[1::3]/rhoL
+    EL   = ul[2::3]
+    pL   = (gamma-1)*(EL-0.5*rhoL*vL*vL)    
+    aL   = np.sqrt(gamma*pL/rhoL)
 
-        # Left side
-        rhoL = ul[i]
-        vL   = ul[i+1]/rhoL
-        EtL  = ul[i+2]
+    rhoR = ur[0::3]
+    vR   = ur[1::3]/rhoR
+    ER   = ur[2::3]
+    pR   = (gamma-1)*(ER - 0.5*rhoR*vR*vR)
+    aR   = np.sqrt(gamma*pR/rhoR)
 
-        # Right side
-        rhoR = ur[i]
-        vR   = ur[i+1]/rhoR
-        EtR  = ur[i+2]
-        
-        # Pressure
-        pL = (gamma-1)*(EtL - 0.5*rhoL*vL*vL)
-        pR = (gamma-1)*(EtR - 0.5*rhoR*vR*vR)
-        
-        # Sound speed
-        aL = np.sqrt(gamma*pL/rhoL)
-        aR = np.sqrt(gamma*pR/rhoR)
-        
-        # Find the maximum eigenvalue
-        maxvap = np.max([np.fabs(vL)+aL, np.fabs(vR)+aR])
+    # Find the maximum eigenvalue for each interface
+    maxvap = np.maximum(np.fabs(vL)+aL,np.fabs(vR)+aR)
 
-        # first: fx = rho*u
-        F[i] = 0.5*(rhoL*vL + rhoR*vR - maxvap*(rhoR-rhoL))
-        
-        # second: fx = rho*u*u+p
-        F[i+1] = 0.5*(rhoL*vL*vL+pL  + rhoR*vR*vR+pR - maxvap*(rhoR*vR-rhoL*vL))
-        
-        # third: fx = (Et+p)*u
-        F[i+2] = 0.5*((EtL+pL)*vL + (EtR+pR)*vR - maxvap*(EtR-EtL))
-                                                                
+    # first: fx = rho*u
+    F[0::3] = 0.5*(rhoL*vL + rhoR*vR - maxvap*(rhoR-rhoL))
+    
+    # second: fx = rho*u*u+p
+    F[1::3] = 0.5*(rhoL*vL*vL+pL  + rhoR*vR*vR+pR - maxvap*(rhoR*vR-rhoL*vL))
+    
+    # third: fx = (E+p)*u
+    F[2::3] = 0.5*((EL+pL)*vL + (ER+pR)*vR - maxvap*(ER-EL))
+
     return F
 
 
